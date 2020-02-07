d920ded25de8689174e09d20e82fab5162c20221
Bartosz Maronka <bartek.maronka@gmail.com>
2020-02-07 14:13:11 +0100

    draw one star feature

--- js/main.js
+++ js/main.js
@@ -1,13 +1,56 @@
-const canvas = document.querySelector('#canvas');
-const ctx = canvas.getContext('2d');
+class Sky {
+    constructor(canvas) {
+        this.canvas = canvas;
+        this.ctx = canvas.getContext('2d');
+        this.width = window.innerWidth;
+        this.height = window.innerHeight;
+    }
 
-ctx.fillStyle = '#ff0000';
-ctx.fillRect(0, 0, 100, 50);
+    initCanvas() {
+        this.canvas.width = this.width;
+        this.canvas.height = this.height;
 
-ctx.beginPath();
-ctx.lineWidth = 3;
-ctx.strokeStyle = '#00ff00';
-ctx.moveTo(50, 50);
-ctx.lineTo(100, 100);
-ctx.lineTo(200, 150);
-ctx.stroke();
+        this.ctx.fillStyle = '#000';
+        this.ctx.fillRect(0, 0, this.width, this.height);
+    }
+
+    drawStar(star) {
+        this.ctx.save();
+
+        this.ctx.fillStyle = star.color;
+
+        this.ctx.beginPath();
+
+        this.ctx.translate(star.x, star.y);
+        this.ctx.moveTo(0, 0 - star.radius);
+
+        for (let i = 0; i < 5; i++) {
+            this.ctx.rotate(Math.PI / 180 * 36);
+            this.ctx.lineTo(0, 0 - star.radius * 0.5);
+            this.ctx.rotate(Math.PI / 180 * 36);
+            this.ctx.lineTo(0, 0 - star.radius);
+        }
+
+        this.ctx.fill();
+        this.ctx.restore();
+    }
+
+    draw() {
+        console.log('draw');
+        window.requestAnimationFrame(() => this.draw());
+    }
+
+    run() {
+        this.initCanvas();
+        // this.draw();
+        this.drawStar({
+            x: 100,
+            y: 100,
+            color: '#fff',
+            radius: 10,
+        })
+    }
+}
+
+const sky = new Sky(document.querySelector('#canvas'));
+sky.run();
